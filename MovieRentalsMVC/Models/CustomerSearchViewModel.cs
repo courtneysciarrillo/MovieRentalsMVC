f39f3e400cc2b93bfbcd51a5070e2f94e5c2afb4
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-27 16:06:53 -0400

    first commit

---
+++ MovieRentalsMVC/Models/CustomerSearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MovieRentalsMVC.Models
+{
+    public class CustomerSearchViewModel
+    {
+        [DisplayName("Search")]
+        public string SearchText { get; set; }
+        public List<Customer> Results { get; set; }
+    }
+}
---
+++ MovieRentalsMVC/Models/Rental.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalsMVC.Models
+{
+    public class Rental
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime RentalDate { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
